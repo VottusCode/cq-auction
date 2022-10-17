10f91d36a481f8a12313221a371fcaf2adf7d878
May Lilian <vottus@vott.us>
2022-10-18 00:07:08 +0200

    mig

---
+++ packages/backend/prisma/migrations/20221017220637_int/migration.sql
@@ -0,0 +1,15 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_AuctionItem" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "donatedBy" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "currentPrice" INTEGER NOT NULL DEFAULT 0,
+    "sold" BOOLEAN NOT NULL DEFAULT false,
+    "image" TEXT NOT NULL DEFAULT 'placeholder.png'
+);
+INSERT INTO "new_AuctionItem" ("currentPrice", "description", "donatedBy", "id", "image", "sold") SELECT "currentPrice", "description", "donatedBy", "id", "image", "sold" FROM "AuctionItem";
+DROP TABLE "AuctionItem";
+ALTER TABLE "new_AuctionItem" RENAME TO "AuctionItem";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
